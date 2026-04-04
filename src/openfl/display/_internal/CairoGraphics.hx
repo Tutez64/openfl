b873f1ae4f0a6cc5d4ef96640a7b69d51b6df3fc
Enzo Persillet (Tutez) <enzopersillet@gmail.com>
2026-04-04 22:49:03 +0200

    Limit Graphics zoom cache reuse to the OpenGL software fallback path

--- src/openfl/display/_internal/CairoGraphics.hx
+++ src/openfl/display/_internal/CairoGraphics.hx
@@ -1829,7 +1829,7 @@ class CairoGraphics
 	}
 	#end
 
-	public static function render(graphics:Graphics, renderer:CairoRenderer):Void
+	public static function render(graphics:Graphics, renderer:CairoRenderer, allowRenderSizeReuse:Bool = false):Void
 	{
 		#if lime_cairo
 		CairoGraphics.graphics = graphics;
@@ -1861,7 +1861,7 @@ class CairoGraphics
 			graphics.__bitmapScaleY = 1;
 		}
 
-		graphics.__update(renderer.__worldTransform, pixelRatio);
+		graphics.__update(renderer.__worldTransform, pixelRatio, allowRenderSizeReuse);
 
 		var width = graphics.__renderWidth;
 		var height = graphics.__renderHeight;
